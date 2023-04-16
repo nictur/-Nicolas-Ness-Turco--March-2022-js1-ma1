d2e5b264525442ffb35ed808e8e35e0b6d448b58
nictur <112319517+nictur@users.noreply.github.com>
2023-04-16 14:27:25 +0200

    homework

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
-];
+// Question 1
+//Awsner 1
+
+const cats = {
+        complain: function() {
+            console.log ("Meow!");
+        }
+};
+
+cats.complain();
+
+
+// Question 2
+//Awsner 2
+
+document.querySelector("h3")
+
+const heading = document.querySelector("h3")
+
+heading.innerHTML = "Updated Heading"
+
+//Quetsion 3 Question 3: Use the style property on the heading variable from the question above to change its font size to "2em".
+// Awsner 3
+
+
+//Quetsion 4 Add a class to the heading variable called subheading..
+// Awsner 4
+
+const heading = document.querySelector("h3")
+
+heading.className = "Subheading";
+
+
+//Quetsion 5 Write code that selects all the p elements on a page and assigns them to a variable called paragraphs. Loop through the p elements and change the colour of each to "red"
+// Awsner 5
+
+document.querySelectorAll(p);
+const p = document.querySelector(p)
+
+
+
