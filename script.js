3e6ee57cb820c3825e59417ae722a98c253e06ab
nictur <112319517+nictur@users.noreply.github.com>
2023-04-16 21:04:39 +0200

    Last changes

--- script.js
+++ script.js
@@ -19,23 +19,87 @@ const heading = document.querySelector("h3")
 
 heading.innerHTML = "Updated Heading"
 
-//Quetsion 3 Question 3: Use the style property on the heading variable from the question above to change its font size to "2em".
+//Quetsion 3
 // Awsner 3
 
 
-//Quetsion 4 Add a class to the heading variable called subheading..
+//Quetsion 4
 // Awsner 4
 
 const heading = document.querySelector("h3")
 
+consol.dir(heading.className);
+
 heading.className = "Subheading";
+consol.log(heading.className);
 
 
-//Quetsion 5 Write code that selects all the p elements on a page and assigns them to a variable called paragraphs. Loop through the p elements and change the colour of each to "red"
+//Quetsion 5 
 // Awsner 5
 
 document.querySelectorAll(p);
-const p = document.querySelector(p)
+const p = document.getElementsByClassName('paragraphs');
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.color = 'red'
+}
+
+//Quetsion 6 
+// Awsner 6
+
+document.querySelector(".results")
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = '<p>New paragraph</p>';
+resultsContainer.style.backgroundColor = 'yellow';
+
+console.log(resultsContainer);
+
+
+//Quetsion 7 
+// Awsner 7
+
+function printListItems(list) { 
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i]);
+
+    }
+ }
+
+ printListItems(cats);
+
+
+//Quetsion 8 
+// Awsner 8
+
+
+function createCats(cats) {
+
+    let listItem = "";
+
+    for(let i = 0; i < cats.length; i++) {
+        console.log(cats[i]);
+
+        listItems = listItem + "<li>" + cats[i] + "</li>";
+        console.log(listItem);
+    }
+
+    const finalHtml = "<ul>" + listItem + "</ul>";
+
+    console.log(finalHtml);
+
+    return finalHtml;
+
+}
+
+const newHtml = createCats(cats);
+
+const catContainer = document.querySelector(".cat-container");
+    catContainer.innerHTML = newHtml;
+    
+
+
+
 
 
 
